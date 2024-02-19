307a0b42a5246b09f00d5b3bbe14180f811ac957
Barbramwendwa <115483620+Barbramwendwa@users.noreply.github.com>
2024-02-19 21:09:04 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -5,18 +5,18 @@ const Education = () => {
     <section className="light" id="education">
       <h2 style={{ textAlign: "center" }}>Education</h2>
 
-      <div className="container" style={{display:"flex", justifyContent:"center"}}>
+    <div className="container" style={{display:"flex", justifyContent:"center"}}>
         <div className="box">
-            <h4>Kaaga Girls' High School</h4>
-            <p>Science and Language</p>
-            <p>2018-2021</p>
+                <h4>Kaaga Girls&apos; High School</h4>
+                <p>Science and Language</p>
+                <p>2018-2021</p>
         </div>
         <div className="box">
-          <h4> Machakos University</h4> 
-            <p> Bachelor of Science in Civil Engineering</p>
-            <p> 2022 - 2027</p>
-            </div>
-      </div>
+            <h4> Machakos University</h4> 
+                <p> Bachelor of Science in Civil Engineering</p>
+                <p> 2022 - 2027</p>
+                </div>
+    </div>
     </section>
   );
 };
